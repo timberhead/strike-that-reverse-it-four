b53316d8ce850a3d283f4f159f7d204b3bb19f89
Al Wong <alwong.@Als-MacBook-Pro.local>
2022-08-11 22:08:47 -0600

    again

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,30 @@ var currentQuestionIndex = 0;
 var choicesEl = document.querySelector("#choices")
 
 
-//array is a list of ojects 
+ 
 
 var question = [{
     questionTitle: "Whats the tallest mountain in Colorado?",
     choices: ["Pikes Peak", "Mount Elbert", "Haystack Mountain", "Longs Peak"],
     answer: "Mount Elbert"
-}, {
+},{
     questionTitle: "Whats the shortest mountain in Colorado?",
     choices: ["Pikes Peak", "Mount Elbert", "Haystack Mountain", "Longs Peak"],
     answer: "Haystack Mountain"
-}, {
+},{
+    questionTitle: "Whats the purplest mountain in Colorado?",
+    choices: ["Pikes Peak", "Mount Elbert", "Haystack Mountain", "Longs Peak"],
+    answer: "Pikes Peak"
+},{
+    questionTitle: "Whats the deadliest mountain in Colorado?",
+    choices: ["Pikes Peak", "Mount Elbert", "Haystack Mountain", "Longs Peak"],
+    answer: "Longs Peak"
 
+},{
+    questionTitle: "Whats the softest mountain in Colorado?",
+    choices: ["Pikes Peak", "Mount Elbert", "Rabbit Mountain", "Longs Peak"],
+    answer: "Rabbit Mountain"
 }
-
 ]
 
 function checkAnswer (){
@@ -41,7 +51,7 @@ function getquestion() {
     console.log(currentQuestion.questionTitle)
     choicesEl.innerHTML = "";
 
-    for (var i = 0; i < currentQuestion.choices.length; i++) {
+    for (var i = 0; i < currentQuestion.choices.length; i++) {      //  what's wrong with length
 
         var choiceBtn = document.createElement("button");
         choiceBtn.setAttribute("value", currentQuestion.choices[i]);
@@ -59,6 +69,40 @@ function getquestion() {
 
 }
 
+function startTimer(duration, display){
+    var timer = duration, stop, minutes, seconds;
+    setInterval(function () {
+
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes <10 ? "0" + minutes : minutes;
+        seconds = seconds <10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;      // what's wrong here
+
+        if (--timer < 0){
+
+            timer = duration;
+        }
+
+        // if (--timer = 0){
+
+        //     timer = stop;
+        // } 
+
+    }, 1000);
+
+
+}
+
+window.onload = function(){
+
+    var fiveMinutes = 60 * 5,
+    display = document.querySelector("#time");
+    startTimer(fiveMinutes, display);
+};
+
 function startQuiz() {
     var startScreenEl = document.querySelector("#start-screen");
     startScreenEl.setAttribute("class", "hide");
@@ -66,11 +110,11 @@ function startQuiz() {
     console.log("start");
 
     getquestion()
-    startClock()
+    startTimer()
 }
 
 
-startBtnEl.addEventListener("click", startQuiz);
+startBtnEl.addEventListener("click", startQuiz, startTimer);    //  why doesn't this work
 
 
 
